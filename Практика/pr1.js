bfe64a42ad87c7ac6a415fcae409ae7eb4061995
Stas <follooowgod@gmail.com>
2024-01-18 22:24:22 +0900

    add pr1.js

---
+++ Практика/pr1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt('Один из последних просмотренный фильмов');
+const b = prompt('На сколько вы его оцените?');
+const c = prompt('Один из последних просмотренный фильмов');  
+const d = prompt('На сколько вы его оцените?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
