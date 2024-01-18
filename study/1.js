8032ab29103a5fa758860a13756428f131ba3843
Stas <follooowgod@gmail.com>
2024-01-18 18:36:13 +0900

    first message

---
+++ study/1.js
@@ -0,0 +1,64 @@
+'use strict';
+
+
+let number = 5;
+
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+	a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+{
+	var result = 50;
+}
+console.log(result);
+
+
+// Урок 10
+
+let num = 4.6;
+
+console.log(4/0);
+
+
+console.log('string' * 9); 
+// NaN не число
+
+const persone = 'Alex';
+// Строка
+
+const bool = false;
+// Правда/ложь
+
+let und;
+console.log(und);
+// undefind отсутствие значения
+
+// Объекты
+const obj1 = {
+	name: 'Jonh',
+	age: 25,
+	isMarried: false
+};
+
+
+console.log(obj.name);
+
+
+
+// Разница между массивами и оюъктами
+
+const arr = [1, 2, 3];
+
+const obj2 = {a:50, b:2};
+
+
+
