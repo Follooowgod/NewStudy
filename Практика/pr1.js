5ed19257a28ec7f3f4af6134c544610b4b85e153
Stas <follooowgod@gmail.com>
2024-02-01 13:19:41 +0900

    edit pr1

--- Практика/pr1.js
+++ Практика/pr1.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренный фильмов');
-const b = prompt('На сколько вы его оцените?');
-const c = prompt('Один из последних просмотренный фильмов');  
-const d = prompt('На сколько вы его оцените?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		  b = prompt('На сколько вы оцените его?', '');
 
+
+	if (a != null && b != null && a!='' && b!='' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+}
+
+if (personalMovieDB.count <= 10) {
+	console.log('Просмотрено довольно мало фильмов');; ('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30 ) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+};
 console.log(personalMovieDB);
