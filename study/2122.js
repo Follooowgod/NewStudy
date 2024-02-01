b461b66a691b5862234665f19d6b21d811cae3d5
Stas <follooowgod@gmail.com>
2024-02-01 12:02:55 +0900

    add 2122

---
+++ study/2122.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const hamburger = true;
+const fries = false;
+
+if(hamburger && fries) {
+    console.log("Я сыт");
+} else {
+    console.log("Я голоден");
+}
+
+console.log((hamburger && fries)); 
