95b04302afcbe003760f0ac1387e89d2e2628c3e
Stas <follooowgod@gmail.com>
2024-02-06 23:32:11 +0900

    edit 2526

--- study/2526.js
+++ study/2526.js
@@ -1,1 +1,53 @@
 'use strict';
+
+let num = 20;
+
+function showFirstMessage (text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+showFirstMessage('Hello World!');
+console.log(num);
+
+
+// function calc(a,  b){
+//     return(a + b);
+// }
+
+// console.log(calc (4, 3));
+// console.log(calc (5, 6));
+// console.log(calc (1354, 5656));
+
+
+
+
+function ret() {
+    let num = 50;
+
+    //
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
+
+
+const calc = (a, b) => {return a + b};
+
+const usdCurr = 28;
+const eurCurr = 32;
+
+function convert (amount) {
+    console.log(curr * amount);
+}
+
+convert (500, usdCurr);
